272317a593b05cbfbaedaff80c4dd5084b89094e
Amanda Downie <amanda@Amandas-MBP.home>
2016-04-26 20:59:26 -0400

    styling arrows

--- wp-content/themes/koenda-pro/template-article-child.php
+++ wp-content/themes/koenda-pro/template-article-child.php
@@ -19,33 +19,37 @@ get_header(); ?>
 								<p><?php the_content(); ?></p>
 
 							</div>
-								<?php
-								$pagelist = get_pages('sort_column=menu_order&sort_order=asc');
-								$pages = array();
-								foreach ($pagelist as $page) {
-								   $pages[] += $page->ID;
-								}
-								
-								$current = array_search(get_the_ID(), $pages);
-								$prevID = $pages[$current-1];
-								$nextID = $pages[$current+1];
-								?>
-								
-								<div class="navigation">
-								<?php if (!empty($prevID)) { ?>
-								<div class="alignleft">
-								<a href="<?php echo get_permalink($prevID); ?>"
-								  title="<?php echo get_the_title($prevID); ?>"><?php echo get_the_title($prevID); ?></a>
-								</div>
-								<?php }
-								if (!empty($nextID)) { ?>
-								<div class="alignright">
-								<a href="<?php echo get_permalink($nextID); ?>" 
-								 title="<?php echo get_the_title($nextID); ?>"><?php echo get_the_title($nextID); ?></a>
-								</div>
-								<?php } ?>
-								</div><!-- .navigation -->								
 						</li>
+						<div class="page-navigation">
+							<?php
+							// $pagelist = get_pages('sort_column=menu_order&sort_order=asc');
+							$pagelist = get_pages( array('sort_column' => 'menu_order') );
+							$pages = array();
+							foreach ($pagelist as $page) {
+							   $pages[] += $page->ID;
+							}
+							
+							$current = array_search(get_the_ID(), $pages);
+							$prevID = $pages[$current-1];
+							$nextID = $pages[$current+1];
+							?>
+							
+							<div class="navigation">
+							<?php if (!empty($prevID)) { ?>
+							<div class="alignleft">
+							<a href="<?php echo get_permalink($prevID); ?>"
+							  title="<?php echo get_the_title($prevID); ?>"><?php echo get_the_title($prevID); ?></a>
+							</div>
+							<?php }
+							if (!empty($nextID)) { ?>
+							<div class="alignright">
+							<a href="<?php echo get_permalink($nextID); ?>" 
+							 title="<?php echo get_the_title($nextID); ?>"><?php echo get_the_title($nextID); ?></a>
+							</div>
+							<?php } ?>
+							</div><!-- .navigation -->	
+						</div>							
+						<div class="clearfix"></div>
 						<hr />
 						 <div class="subnav"><?php echo koenda_article_subnav(); ?></div>
 					<?php endwhile; ?>
