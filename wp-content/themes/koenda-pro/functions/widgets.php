3f77ea892d7b38784f8c90644af5b3af8b855c2b
Amanda Downie <amanda@Amandas-MBP.home>
2016-05-01 19:03:09 -0400

    minor styles for contact sidebar, adding contact sidebar

--- wp-content/themes/koenda-pro/functions/widgets.php
+++ wp-content/themes/koenda-pro/functions/widgets.php
@@ -20,6 +20,15 @@ function koenda_widgets_init() {
 		'after_title' => '</h2>',
 	));
 	register_sidebar(array(
+		'name' => __( 'Contact Sidebar', "Koenda"),
+		'id' => 'contact-sidebar',
+		'description' => __( 'The sidebar on the Contact page', "Koenda"),
+		'before_widget' => '<div class="widget">',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	));	
+	register_sidebar(array(
 		'name' => __( 'Footer Widget Area 1', "koenda"),
 		'id' => 'footer-widget-area-1',
 		'description' => __( 'The footer widget area 1', "koenda"),
@@ -74,4 +83,92 @@ function koenda_widgets_init() {
 }
 endif;
 add_action( 'widgets_init', 'koenda_widgets_init' );
+
+
+/**
+ * Simplified variant of the native text widget class.
+ *
+ * @author Thomas Scholz aka toscho http://toscho.de
+ * @version 1.0
+ */
+class Unfiltered_Text_Widget extends WP_Widget
+{
+    /**
+     * @uses apply_filters( 'magic_widgets_name' )
+     */
+    public function __construct()
+    {
+        // You may change the name per filter.
+        // Use add_filter( 'magic_widgets_name', 'your custom_filter', 10, 1 );
+        $widgetname = apply_filters( 'magic_widgets_name', 'Unfiltered Text' );
+        parent::__construct(
+            'unfiltered_text'
+        ,   $widgetname
+        ,   array( 'description' => 'Pure Markup' )
+        ,   array( 'width' => 300, 'height' => 150 )
+        );
+    }
+
+    /**
+     * Output.
+     *
+     * @param  array $args
+     * @param  array $instance
+     * @return void
+     */
+    public function widget( $args, $instance )
+    {
+        echo $instance['text'];
+    }
+
+    /**
+     * Prepares the content. Not.
+     *
+     * @param  array $new_instance New content
+     * @param  array $old_instance Old content
+     * @return array New content
+     */
+    public function update( $new_instance, $old_instance )
+    {
+        return $new_instance;
+    }
+
+    /**
+     * Backend form.
+     *
+     * @param array $instance
+     * @return void
+     */
+    public function form( $instance )
+    {
+        $instance = wp_parse_args( (array) $instance, array( 'text' => '' ) );
+        $text     = format_to_edit($instance['text']);
+?>
+        <textarea class="widefat" rows="7" cols="20" id="<?php
+            echo $this->get_field_id( 'text' );
+        ?>" name="<?php
+            echo $this->get_field_name( 'text' );
+        ?>"><?php
+            echo $text;
+        ?></textarea>
+        <?php
+        /* To enable the preview uncomment the following lines.
+         * Be aware: Invalid HTML may break the rest of the site and it
+         * may disable the option to repair the input text.
+
+        ! empty ( $text )
+            and print '<h3>Preview</h3><div style="border:3px solid #369;padding:10px">'
+                . $instance['text'] . '</div>';
+        /**/
+        ?>
+<?php
+    }
+}
+
+add_action( 'widgets_init', 'register_unfiltered_text_widget', 20 );
+
+function register_unfiltered_text_widget()
+{
+    register_widget( 'Unfiltered_Text_Widget' );
+}
 ?>
